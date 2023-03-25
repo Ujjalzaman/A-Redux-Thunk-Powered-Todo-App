43173d877beffafc9dd2a7eb18b4f2f75a2b154e
ujjalz <ujjalz@thegates.biz>
2023-03-26 02:05:54 +0600

    Implemented fetching todo with middlware concept in vanilla js

---
+++ Redux-thunk-in-vanilla-js/index.js
@@ -0,0 +1,50 @@
+
+const {delayMiddleware,fetchMiddlewares } = require('./todoMiddleware');
+const { createStore, applyMiddleware } = require("redux");
+// initialState
+const initialState = {
+    todos : [],
+}
+
+// reducer 
+const todoReducer = (state=initialState, action) =>{
+    switch (action.type) {
+        case 'todo/todoAdded':
+            return {
+                ...state,
+                todos: [
+                    ...state.todos,
+                    {
+                        title: action.payload
+                    }
+                ]
+            }
+        case 'todo/todoLoaded':
+            return {
+                ...state,
+                todos: [...state.todos, ...action.payload]
+            }
+    
+        default:
+            state;
+    }
+}
+
+// store
+const store = createStore(todoReducer, applyMiddleware(delayMiddleware,fetchMiddlewares));
+
+// subscribe
+store.subscribe(() =>{
+    console.log(store.getState())
+})
+
+// action to subscribe
+store.dispatch({
+    type: 'todo/todoAdded',
+    payload: 'Adding someting speacial'
+})
+
+store.dispatch({
+    type: 'todo/fetchTodo',
+    payload: 'Adding someting speacial'
+})
---
+++ Redux-thunk-in-vanilla-js/todoMiddleware.js
@@ -0,0 +1,31 @@
+const { default: fetch } = require("node-fetch");
+
+const delayMiddleware = (store) => (next) => (action) =>{
+    if(action.type === 'todo/todoAdded'){
+        console.log('i m delaying to you');
+
+        setTimeout(() => {
+            next(action)
+        }, 2000);
+        return ;
+    }
+    return next(action)
+}
+const fetchMiddlewares = (store) => (next) => async(action) => {
+    if(action.type === 'todo/fetchTodo'){
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+        const todos = await response.json();
+        store.dispatch({
+            type: 'todo/todoLoaded',
+            payload: todos
+        })
+        console.log(`geeting data is: ${store.getState().todos.length}`)
+        return ;
+    }
+    return next(action)
+}
+
+module.exports = {
+    delayMiddleware,
+    fetchMiddlewares
+}
