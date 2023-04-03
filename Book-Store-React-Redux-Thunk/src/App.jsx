fd070c70b56fd978dce70bb63f5a42a9b53a3560
ujjalzaman <muzzwal97@gmail.com>
2023-04-03 22:48:12 +0600

    Initalize User interface of BookStore website

---
+++ Book-Store-React-Redux-Thunk/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from './component/Nav'
+import BookList from './component/BookList'
+import AddNewFrom from './component/AddNewFrom'
+
+function App() {
+
+  return (
+    <>
+      <Nav/>
+      <main className="py-12 2xl:px-6">
+          <div className="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
+              <BookList/>
+              <AddNewFrom/>
+          </div>
+      </main>
+    </>
+  )
+}
+
+export default App
